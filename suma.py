5413b85ac9d655986f0fb84bea99c2e8382fac21
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-04-15 22:36:45 -0600

    Ejemplo del operador de suma en Python

---
+++ suma.py
@@ -0,0 +1,9 @@
+print("Bienvenido")
+
+n1=int(input("Ingresa un numero "))
+n2=int(input("Ingresa otro nunero "))
+
+print("Los numeros ingresados son los siguientes",n1 ,n2)
+suma=(n1+n2)
+
+print("La suma de ambos numeros es: ",suma)
