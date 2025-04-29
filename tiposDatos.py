8e82424794305feb9ce5c0fc19ae9bbc41be6cd9
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-04-29 06:23:40 -0600

    Mas tipos de datos y ejemplos

--- tiposDatos.py
+++ tiposDatos.py
@@ -10,3 +10,13 @@ print(type(numero))
 numero2=2.5
 print("Lo siguiente es un numero de tipo de dato float: \n",numero2)
 print(type(numero2))
+
+#tipo string o cadenas de caracteres
+cadena='hola'
+print(cadena)
+print(type(cadena))
+
+#tipo de datos BOOLEANOS 
+booleano=True 
+print(booleano)
+print(type(booleano))
