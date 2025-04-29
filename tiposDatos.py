bad55455ff59965d94b2dc079c1da9bc859b62f9
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-04-28 22:23:01 -0600

    Se agrego otros tipo de dato

--- tiposDatos.py
+++ tiposDatos.py
@@ -4,3 +4,9 @@
 numero=50
 print(numero)
 print(type(numero))
+
+#tipo float o punto flotante
+#ejemplo 
+numero2=2.5
+print("Lo siguiente es un numero de tipo de dato float: \n",numero2)
+print(type(numero2))
