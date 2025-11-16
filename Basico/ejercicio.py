a11fe88b74aefde83c2d38023642ff53e9a8f16c
Diego Arturo Noguez Lopez <154560222+DiegoNoguez@users.noreply.github.com>
2025-11-15 22:20:26 -0600

    Actualizar ejercicio.py

--- Basico/ejercicio.py
+++ Basico/ejercicio.py
@@ -1,3 +1,3 @@
-""" MOstrrar por consola el nombre, pais  y edad"""
+""" MOstrrar por consola el nombre, pais  y edad ejercicio basico de impresion en python usando caracteres especiales para salto dd linea en la misma instruccion de impresion."""
 
 print("Diego Noguez, \nMéxico \n20Años")
