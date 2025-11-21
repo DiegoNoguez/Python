80b18c9605193ac0bd684b9e11958c8013b3b144
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-11-20 22:43:22 -0600

    Ejemplo de conversion de datos

---
+++ entradaDatos/ejempli_tipos.py
@@ -0,0 +1,20 @@
+# Ejemplo de tipos de datos
+
+# Ejemplo de concatenacion o suma de valores
+num1String = '10'
+num2String = '20'
+
+print(f'Número 1 en cadena {num1String}')
+print(f'Número 2 en cadena {num2String}')
+
+resultado = num1String + num2String
+
+print(f'Concatenación: {resultado}')
+
+# cONVERSION A ENTEROS para realizar una operacion aritmetica 
+num1 = int(num1String)
+num2 = int(num2String)
+
+resultado =  num1 + num2
+
+print(f'Suma de los valoes: {resultado}')
