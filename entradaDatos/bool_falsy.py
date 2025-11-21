f7af49132f02e54d22f55ae129c5540f1ca13d66
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-11-20 22:33:26 -0600

    Se agrega contenido de bool

---
+++ entradaDatos/bool_falsy.py
@@ -0,0 +1,14 @@
+# Uso de las funciones bool y falsy en python con estas fucniones
+# y como usarlos con ejemplos 
+
+# Funcion bool 
+print(f"valor 0: {bool(0)}") # Es considerado falsa y si es 1 true 
+print(f"valor 0: {bool(1)}")
+
+print(f"valor '':{bool('')}") # cadena vacia es falso
+print(f"valor 'HOLa:{bool('Hola')}") # cadena con caraccteres es true 
+
+print(f"Valor none: {bool(None)}") # Recibira o dara un falseya que se considera ausencia de valor
+
+
+# funcion falsy 
