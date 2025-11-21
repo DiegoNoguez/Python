aff622cd8b1ebc206d11e48b7a3d418451b474dd
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-11-20 23:06:22 -0600

    Sistema empleados

---
+++ entradaDatos/sistemaEm.py
@@ -0,0 +1,17 @@
+""" SISTEMA DE EMPLEADO 
+    Crear un programa para solicitar la informacion de un 
+    empkeado, introduciendo  los daros por consola 
+        Nombre empleado 
+        edad del empleado 
+        Slario del empleado 
+        Es jefe de departamento 
+    """
+
+# Definición de variables
+dis = "-"
+
+# Desarollo de la logica
+disRep = dis * 3
+
+# Impresion de los datos
+print(f'{disRep} Sistema de Empelados {disRep}')
