71a3df5e8398bca699fc8dd23d95875842d03ee4
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-11-20 22:22:56 -0600

    Se ingresa nuevo tema

---
+++ entradaDatos/conversion.py
@@ -0,0 +1,16 @@
+# Conversion de tipos de datos en python 
+
+numeroString = "25"
+
+# Convertir la cadena a tipo entero
+numeroEntero = int(numeroString)
+print(numeroString)
+print(numeroEntero)
+
+# Conversion de un nuemero entero a float 
+numeroFlotante = float(numeroEntero)
+print(numeroFlotante)
+
+# Conversion de tipo flotante a string
+numeroTexto = str(numeroFlotante)
+print(numeroTexto)
