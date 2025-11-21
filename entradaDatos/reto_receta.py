3bd603fe96390d83587ef27d4e70492985b372ca
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-11-21 08:37:59 -0600

    Inicio de ejercicio

---
+++ entradaDatos/reto_receta.py
@@ -0,0 +1,12 @@
+""" 
+    RETO DE RECETA DE COCINA 
+    Crear un programa para solicitar algunos valores importantes para 
+    una receta de cocina
+    Los valores que debe introducir el usuario son :
+        Nombre de la receta 
+        Ingredientes 
+        Tiempo de preparación 
+        dificultad ('FACIL, MEDIANA, ALTA')
+
+    Mandar a imprimir la receta 
+"""
