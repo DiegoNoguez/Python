0ee4fbb5e977231eed6dbaea06224187ecc948f1
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-11-20 22:51:17 -0600

    Se completa el codigo

--- entradaDatos/entradaConsola.py
+++ entradaDatos/entradaConsola.py
@@ -1,1 +1,8 @@
 # Entrada de datos por consola 
+
+nombre= input("Escribe tu nombre: ")
+edad = int(input("Escribte tu edad: "))
+
+print(f'Hola mi nombre es: {nombre}')
+print(f'Mi edad es: {edad}')
+print(f'En un año tendre: {edad+1}')
