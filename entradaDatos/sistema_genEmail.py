8a9314a1de226ad8a4e1f719b7e56ec29cd8eab7
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-11-21 23:26:27 -0600

    Finalizacion del ejercicio

--- entradaDatos/sistema_genEmail.py
+++ entradaDatos/sistema_genEmail.py
@@ -9,19 +9,23 @@
     email: nombreusuario@empresa.dominio
     Los datos mostrados son de ejemplo y solo 
     se usa para reforzar el ejercicio de funciones con cadenas o Strings 
+    Con entrada de datos por consola 
     """
 # Inicio del programa 
 print(f'<----- Sistema Generador de Email ----->')
 nombreUsurio = input("Ingresa tus nombre o nombres: ")
 apellido = input("Ingresa tus Apellidos: ")
-nombreEmpresa = " SpringyTec SOlutions "
+nombreEmpresa = " SpringyTec Solutions "
 extensionDominio  = " .com.mx "
 
 # Generacion de la logica
 nombreCompleto = nombreUsurio + apellido
-nombreCompleto = nombreCompleto.strip().lower().replace(' ','.') 
+usuario = nombreCompleto.strip().lower().replace(' ','.') 
+nombreEmpresa = nombreEmpresa.strip().lower().replace(' ','')
+dominio = extensionDominio.strip()
 
 # Impresion de lso datos 
 print(f'\nNombre de usuario: {nombreCompleto}')
-print(f'Nombre de la emprea: ')
-print(f'Corrreo Final: ')
+print(f'Nombre de la emprea: {nombreEmpresa}')
+print(f'Estensión o dominio: {dominio}')
+print(f'Corrreo Final:{usuario}@{nombreEmpresa}{dominio} ')
