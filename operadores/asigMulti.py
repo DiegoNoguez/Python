6b9b36c42e3ed7858e4805dda1c4b4c16405a978
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-11-22 21:30:22 -0600

    Operadores y ejemplos

---
+++ operadores/asigMulti.py
@@ -0,0 +1,10 @@
+# Operadores de asignanción multiple
+x, y , z =4 , 3, "Adios"
+
+print(f'El valor de x es {x}')
+print(f'El valor de Y es {y}')
+print(f'EL valor de z es {z}')
+
+# Asiganaciion encadena 
+contador = contador1 = 0
+print(f'Valor de los contadores {contador} y {contador1}')
---
+++ operadores/opeAritmeticos.py
@@ -0,0 +1,32 @@
+# Operadores aritmeticos 
+a = 10
+b = 5
+
+# Suma 
+suma = a + b
+ 
+# Resta
+resta = a - b
+
+# Multiplicacion
+multi = a * b
+
+# Division
+div = a / b
+
+# Division entera
+divEn = a // b
+
+# modulo 
+modulo = a % b
+
+# Exponente
+exponente = a ** b
+# Resultados 
+print(f'EL resultado de la suma es: {suma}')
+print(f'EL resultado de la resta es: {resta}')
+print(f'EL resultado de la multipliación es: {multi}')
+print(f'EL resultado de la divison es: {div}')
+print(f'EL resultado de la Divion Entera es: {divEn}')
+print(f'EL resultado de la Residuo de la división es: {modulo}')
+print(f'EL resultado de la Exponente o potencia es: {exponente}')
