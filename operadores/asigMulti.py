aff422b4f6c90596a95e06b9d6d0a7cb50617b16
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-11-22 21:35:49 -0600

    NUevos Ejemplos

--- operadores/asigMulti.py
+++ operadores/asigMulti.py
@@ -8,3 +8,13 @@ print(f'EL valor de z es {z}')
 # Asiganaciion encadena 
 contador = contador1 = 0
 print(f'Valor de los contadores {contador} y {contador1}')
+
+
+# Ejemplo 
+# intercambio de valores de una varibale sin usar variables temporales
+# Usando el concepto de asignacion multiple 
+x, y = 5, 10
+print(f'Valores originales de x, y {x}, y y vale{y}')
+
+x, y = y, x 
+print(f'Valores dado de x, y {y}, y x vale{x}')
