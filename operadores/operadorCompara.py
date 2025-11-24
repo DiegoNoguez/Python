50633753ae3c6c8d09da96c8060ee6ad8c8215c8
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-11-23 23:49:04 -0600

    Uso de comparacion

---
+++ operadores/operadorCompara.py
@@ -0,0 +1,13 @@
+# operadores de comparacion y algunos ejemplso
+a,b = 7, 5
+
+# impresion de valores inicioales 
+print(f'Valor inicial de a: {a}, b: {b}')
+
+# OPerador de igualdad ==
+reulstado = a==b 
+print(f'Resultado de a==b es: {reulstado}')
+
+# OPerador diferente !=
+reulstado = a!=b
+print(f'A es distinto de b {reulstado}')
