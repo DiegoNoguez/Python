b5365c1a07728fc67fb33779253b9770204c373c
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-11-23 23:34:10 -0600

    Nuevos operadores

---
+++ operadores/operadoresCompuestos.py
@@ -0,0 +1,10 @@
+# Operadores de asignacion compuesto 
+a, b = 10,25
+print(f'Valor inicial a: {a}, b: {b}' )
+
+# OPERADPOR CPMOUESTO DE += 
+a += b
+print(f'Suma de a + b es igual a = {a}')
+
+# Los otrso operadoes se puede hacer con resta, divison, multiplicacion
+# jutno al de modulo y potencia
