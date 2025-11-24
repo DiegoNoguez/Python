eec2ee218b1d12f82d4a7ee2ad4b89e8d1237f10
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-11-23 23:55:11 -0600

    Operador and y ejemplo de uso

---
+++ operadores/OperadorAnd.py
@@ -0,0 +1,7 @@
+# Operadores logicos. 
+#Operador and 
+# Rretionra verdaderos si ambos son verdaderos
+cond= False
+cond2 = False
+resultado = cond and cond2 
+print(f'El resultado si el valor de condicion1 {cond} y condicion2 {cond2}  por lo tanto el resultado es {resultado}')
