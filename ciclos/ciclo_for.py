62d13759860c4b7e497e5f0b34bb08d7bda483d4
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-12-08 19:32:20 -0600

    Se agrega ejemplo del ciclo for

---
+++ ciclos/ciclo_for.py
@@ -0,0 +1,9 @@
+# Ejemplo de como suar el cilco for en python 
+
+# Impresion de las tablas del 2
+numero = 2
+print(f'La tabla del multiplicar del 2')
+
+for i in range(1,11):
+    resultado = numero * i
+    print(f'{i} * {numero} = {resultado}')
