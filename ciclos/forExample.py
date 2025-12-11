4fef1b381505c3b5af1af3557055ddc80c34c146
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-12-10 19:32:57 -0600

    Ejemplo nuevo de for

---
+++ ciclos/forExample.py
@@ -0,0 +1,14 @@
+# Ejemplo mas detalllado del cilco for recordando que  un
+# ciclo for es mas costoso a nivel de hardware 
+
+print("Ciclo for")
+
+cadena = 'Hola Mundo'
+for letra in cadena:
+    print(letra, end= ' ')
+
+frutas = ['Platano', 'Manzana', 'Mango']
+print('\n')
+
+for fruta in frutas:
+    print(fruta, end=' ')
