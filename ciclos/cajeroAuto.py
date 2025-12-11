f6fc5c09a7edec16779183ca8dfa293cf9ed0188
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-12-10 21:16:29 -0600

    finalizacion de ejercicio

--- ciclos/cajeroAuto.py
+++ ciclos/cajeroAuto.py
@@ -7,6 +7,7 @@ como funciones principales de un cajero, deposito, retiro, consultar el saldo.,
 
 print("<--- Aplicación de Cajero Automatico --->")
 
+SALDO = 1000
 salida = False 
 while not salida:
     print(''' Operaciones que puedes realizar:
@@ -15,3 +16,21 @@ while not salida:
           3.- Depositar
           4.- Salir''')
     opcion = int(input("Escoge una opción a realizar: "))
+    if opcion == 1:
+        print(f'Su saldo actual es de: {SALDO}\n')
+    elif opcion == 2:
+        retiro = int(input("Ingresa el monto a retirar: "))
+        if retiro <= SALDO:
+            print(f'EL retiro fue exitoso')
+            print(f'La cantidad a retirar fue de: {retiro}')
+        else:
+            print(f'No cuentas con el saldo suficiente en la cuenta para hacer el retiro')
+            print(f'Tu saldo actual es de: {SALDO}\n')
+    elif opcion == 3:
+        deposito = int(input("Ingresa la cantidad a depsoistar: "))
+        SALDO +=deposito
+        print("\n")
+    elif opcion == 4:
+        print(f'Saliendo de la aplicación bancanria')
+        salida = True
+print()
