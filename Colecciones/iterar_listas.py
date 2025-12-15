0432500fea3ccc508cd071e5962882c9b4132081
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-12-15 10:01:51 -0600

    Ejemplo de lustas

---
+++ Colecciones/iterar_listas.py
@@ -0,0 +1,11 @@
+# ITERACION DE LISTAS 
+print(f'    ITERAR LISTAS ')
+nombres = ['Karla', 'Sofia', 'Andrea', 'Fernanda', 'Anlly']
+for nombre in nombres:
+    print(nombre)
+
+lista_heterogenea = [100, True, 'Ivan']
+print()
+for elemento in lista_heterogenea:
+    print(elemento)
+    
