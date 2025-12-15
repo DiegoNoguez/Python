fe9aba87ebdf16d578afc3d8b627e352e5e3efdf
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-12-15 06:26:52 -0600

    Nuevo tema y ejercicio

---
+++ Colecciones/listas.py
@@ -0,0 +1,10 @@
+print('Manejo de listas')
+mi_lista = [1,2,3,4]
+print(f'{mi_lista} Esta es la lista original')
+
+# Obtencion de lo largo de una lista
+print(f'El largo de la lista es: {len(mi_lista)}')
+
+#Acceder a los elementos de la lista por indice 
+print(f'Accedemos al valor del indice: {len(mi_lista[4])}') # Tambien se puede manejar de forma negativa
+
