0cfd7029870a6ffd23297c7a18e4d49ab7c5d104
DiegoNoguez <noguezlopezarturo@gmail.com>
2026-04-09 11:17:00 -0600

    Archivos modificados y comentarios de orientacion

--- Uso_Mongo/mongo_test.py
+++ Uso_Mongo/mongo_test.py
@@ -1,8 +1,29 @@
-from pymongo import MongoClient 
+from pymongo import MongoClient  # importacion de la libreria de pymongo para hacer peticiones y  modificaciones a la bd
+
 
 # Establecenos la conexion a la base 
-client = MongoClient("mongodb://localhost:27017")
+#client = MongoClient("mongodb://localhost:27017")
 
+# Linea de conexion cuando se solicita contra de root en mongo 
+client = MongoClient("mongodb://root:1234@localhost:27017/")
 # Probar la conexion 
-print(f'Base de datos disponibles: ')
-print(client.list_database_names())
+#print(f'Base de datos disponibles: ')
+#print(client.list_database_names())
+
+
+# Creacion de una BD en mongo 
+
+# Lo siguiente no crea nada de basee de datos aun solo dice con que y como trabjar . 
+# Seleccion de un bd
+db = client["prueba1"]
+
+# Seleccionar coleccion
+user = db["users"]
+
+print("Conectado correctamente")
+
+user = {
+    "nombre":"Diego",
+    "edad"
+}
+
